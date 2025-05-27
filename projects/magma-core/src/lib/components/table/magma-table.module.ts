843bb9a2cc9351d850886bb74d89a700a86cadf9
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2025-05-27 13:25:08 +0200

    change table loading

--- projects/magma-core/src/lib/components/table/magma-table.module.ts
+++ projects/magma-core/src/lib/components/table/magma-table.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MagmaTableComponent } from './magma-table/magma-table.component';
+import { ProgressSpinnerModule } from '../progressSpinner/progress-spinner.module';
 
 
 
@@ -9,7 +10,8 @@ import { MagmaTableComponent } from './magma-table/magma-table.component';
     MagmaTableComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProgressSpinnerModule
   ],
   exports: [
     MagmaTableComponent
--- projects/magma-core/src/lib/components/table/magma-table/magma-table.component.ts
+++ projects/magma-core/src/lib/components/table/magma-table/magma-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'magma-table',
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class MagmaTableComponent {
 
+  @Input() loading = false
+  
 }
